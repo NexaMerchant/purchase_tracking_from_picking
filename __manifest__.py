ddaa3586c1e0af23b93dad455992e55b27aab47f
odoo <nice.lizhi@gmail.com>
2025-05-25 02:42:03 +0000

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Purchase Order Tracking Number",
+    "version": "16.0.1.0.0",
+    "category": "Purchase",
+    "summary": "Display and edit tracking number from incoming picking on purchase order",
+    "depends": ["purchase", "stock"],
+    "data": [
+        "views/purchase_order_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
